830c2b540463ba245236f0b80b830807dbd5cbc3
= <=>
2017-05-20 14:42:04 -0500

    Se adiciona metodo de Time

--- studio_game.rb
+++ studio_game.rb
@@ -1,24 +1,16 @@
-player_name = 'larry\'s'
-player_health = 60
-player_health2 = 125
-player_health3 = 100
-player_name2 = 'curly'
-player_name3 = 'moe'
-player_name4 = "shemp"
-player_health4 = 60
-puts "#{player_name} healht is #{player_health}"
-puts "Players are \n #{player_name} \n #{player_name2} \n #{player_name3}"
-puts "".center(30,"*")
-puts "#{player_name.capitalize} has a healht of #{player_health}"
-puts "#{player_name2.upcase} has a health of #{player_health2}"
-player_health2  = player_health
-puts "#{player_name2.upcase} has a health of #{player_health2}"
-player_health2 = 30
-puts "#{player_name2.upcase} has a health of #{player_health2}"
-puts "".center(30,"-")
-puts "#{player_name3.capitalize} has a healt of #{player_health3}".center(60,"*")
-puts "#{player_name4.capitalize.ljust(40,".")} #{player_health4}"
-puts "".center(100,"*")
-time_example = Time.new
-puts time_example.strftime("The game started on %A %d/%m/%y at %l:%m%p ")
-puts "Hola"
+
+def say_hello(name = "Sin Nombre",health =99)
+     "Hi, I am #{name.capitalize} and my Health is #{health} as of #{current_time}"
+end
+
+def current_time
+    Time.new.strftime("%I:%M:%S %p")
+end
+
+puts say_hello("Adan",100)
+puts say_hello("Ana Sofia",150)
+puts say_hello("Felipe A",160)
+puts say_hello("Andres A")
+
+
+
